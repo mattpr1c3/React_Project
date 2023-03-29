80f989b6055115cb99fc357a524698dda741ae28
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-29 09:03:31 -0700

    did part three of exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import FilmsList from "./components/filmsList";
 
 class App extends Component {
   constructor(props) {
@@ -34,6 +35,7 @@ class App extends Component {
             return <li key={item + idx}>{item}</li>;
           })}
         </ul>
+        <FilmsList />
       </div>
     );
   }
